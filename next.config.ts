71ca8818bd772d344ad316a810200fbe0744c2c7
I-Kovalov <carolinashannon06@proton.me>
2026-01-25 20:48:22 +0200

    netlify 3

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  // output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -13,8 +11,8 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: '127.0.0.1',
-        port: '54321', // Specify the port if needed
-        pathname: '/storage/v1/object/public/images/**', // Adjust the pathname as needed
+        port: '54321',
+        pathname: '/storage/v1/object/public/images/**',
       },
     ],
   },
