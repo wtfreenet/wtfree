eaacc0be8685d3c8241ee0682ce9258242da89d1
I-Kovalov <carolinashannon06@proton.me>
2026-01-28 17:09:28 +0200

    ы

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-2xl font-normal">Site not found</h1>
+      </div>
+    </div>
+  );
+}
